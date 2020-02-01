5e6ee9bf312937952ee3ae1641a44315c079a21b
Shane Lillie <ignignokterr@gmail.com>
2020-02-01 00:27:34 -0800

    Add spine runtime
    Some random work on the player prefab
    Renamed the game scene
    Random work on the viewer

--- Assets/Scripts/ggj2020/Camera/GameViewer.cs
+++ Assets/Scripts/ggj2020/Camera/GameViewer.cs
@@ -1,9 +1,16 @@
-﻿using pdxpartyparrot.Core.Camera;
+﻿using Cinemachine;
+
+using pdxpartyparrot.Core.Camera;
 using pdxpartyparrot.Game.Camera;
 using pdxpartyparrot.ggj2020.Data;
 
+using UnityEngine;
+
 namespace pdxpartyparrot.ggj2020.Camera
 {
+    [RequireComponent(typeof(CinemachineFramingTransposer))]
+    [RequireComponent(typeof(CinemachinePOV))]
+    [RequireComponent(typeof(CinemachineConfiner))]
     public sealed class GameViewer : CinemachineViewer, IPlayerViewer
     {
         public Viewer Viewer => this;
