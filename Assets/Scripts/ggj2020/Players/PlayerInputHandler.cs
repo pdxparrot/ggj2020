23fde330f297215c1eda8bb225a89d07f3731ed5
Shane Lillie <ignignokterr@gmail.com>
2020-02-01 10:30:53 -0800

    Added contextual actoins

--- Assets/Scripts/ggj2020/Players/PlayerInputHandler.cs
+++ Assets/Scripts/ggj2020/Players/PlayerInputHandler.cs
@@ -1,7 +1,9 @@
 ﻿using pdxpartyparrot.Game.Players.Input;
 using pdxpartyparrot.ggj2020.Data.Players;
 
+using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 namespace pdxpartyparrot.ggj2020.Players
 {
@@ -18,6 +20,38 @@ namespace pdxpartyparrot.ggj2020.Players
 
             Assert.IsTrue(PlayerInputData is PlayerInputData);
             Assert.IsTrue(Player is Player);
+        }
+#endregion
+
+#region Actions
+        public void OnInteractAction(InputAction.CallbackContext context)
+        {
+            if(!IsInputAllowed(context)) {
+                return;
+            }
+
+            if(Core.Input.InputManager.Instance.EnableDebug) {
+                Debug.Log($"Interact: {context.action.phase}");
+            }
+
+            if(context.performed) {
+                Debug.LogWarning("TODO: context interact (ladder, tool, etc)");
+            }
+        }
+
+        public void OnCancelAction(InputAction.CallbackContext context)
+        {
+            if(!IsInputAllowed(context)) {
+                return;
+            }
+
+            if(Core.Input.InputManager.Instance.EnableDebug) {
+                Debug.Log($"Cancel: {context.action.phase}");
+            }
+
+            if(context.performed) {
+                Debug.LogWarning("TODO: context cancel (drop tool, etc)");
+            }
         }
 #endregion
     }
