d5d43f5158ce14132bcf8b4ddd2d74ccb4fcd003
Shane Lillie <ignignokterr@gmail.com>
2020-02-01 12:04:02 -0800

    Ladder prefabs
    Ladders in the bay
    Walkway in the bay
    Lower player speed to something not insane

---
+++ Assets/Scripts/ggj2020/World/Ladder.cs
@@ -0,0 +1,13 @@
+﻿using pdxpartyparrot.Core.Actors;
+using pdxpartyparrot.Game.World;
+
+using UnityEngine;
+
+namespace pdxpartyparrot.ggj2020.World
+{
+    [RequireComponent(typeof(Collider))]
+    public class Ladder : Actor3D, IGrabbable
+    {
+        public override bool IsLocalActor => true;
+    }
+}
