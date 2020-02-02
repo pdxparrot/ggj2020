f9926b102e5efe2ffaeb62364ef2ec6f49c182ee
Luis <luismex4@gmail.com>
2020-02-01 19:41:54 -0800

    Made tool physics kinematic

--- Assets/Scripts/ggj2020/Tools/Tool.cs
+++ Assets/Scripts/ggj2020/Tools/Tool.cs
@@ -40,10 +40,6 @@ namespace pdxpartyparrot.ggj2020.Tools
         virtual public void SetHeld(Mechanic player)
         {
             HoldingPlayer = player;
-            // -- parenting 2 rigidbodys is bad news, so remove it
-            Rigidbody rigid = gameObject.GetComponentInParent<Rigidbody>();
-            parent = rigid.gameObject;
-            Destroy(rigid);
             parent.transform.SetParent(player.transform);
         }
 
@@ -51,8 +47,6 @@ namespace pdxpartyparrot.ggj2020.Tools
         {
             HoldingPlayer = null;
             parent.transform.SetParent(null);
-            // -- add a rigid body so we have nice physics
-            parent.AddComponent<Rigidbody>();
             parent = null;
         }
 
