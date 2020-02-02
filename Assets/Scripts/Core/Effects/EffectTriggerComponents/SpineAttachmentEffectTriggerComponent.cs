f7b6b107ecc94dd6c9f0bc1270e942297016c715
Shane Lillie <ignignokterr@gmail.com>
2020-02-02 12:15:07 -0800

    Effect trigger for spine attachments

---
+++ Assets/Scripts/Core/Effects/EffectTriggerComponents/SpineAttachmentEffectTriggerComponent.cs
@@ -0,0 +1,30 @@
+﻿#if USE_SPINE
+using JetBrains.Annotations;
+
+using pdxpartyparrot.Core.Util;
+
+using UnityEngine;
+
+namespace pdxpartyparrot.Core.Effects.EffectTriggerComponents
+{
+    public class SpineAttachmentEffectTriggerComponent : EffectTriggerComponent
+    {
+        [SerializeField]
+        private SpineSkinHelper _skinHelper;
+
+        [SerializeField]
+        private string _slotName = "default";
+
+        [CanBeNull]
+        [SerializeField]
+        private string _attachmentName;
+
+        public override bool WaitForComplete => false;
+
+        public override void OnStart()
+        {
+            _skinHelper.SetAttachment(_slotName, _attachmentName);
+        }
+    }
+}
+#endif
